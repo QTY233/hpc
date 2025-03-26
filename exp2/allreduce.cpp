fd7ef1ec2cf049576099bcceb796900405c348b7
QTY233 <2033545458@qq.com>
2025-03-26 22:06:53 +0800

    more info2

--- exp2/allreduce.cpp
+++ exp2/allreduce.cpp
@@ -35,6 +35,10 @@ void Ring_Allreduce(void* sendbuf, void* recvbuf, int n, MPI_Comm comm, int comm
 
         for (int j = 0; j < step; ++j)
             ((float*)recvbuf)[recv_offset + j] += temp_buf[j];
+
+        std::cerr << "Rank " << my_rank << " recvbuf after iteration " << i << ": ";
+        for (int j = 0; j < n; j++) std::cerr << ((float*)recvbuf)[j] << " ";
+        std::cerr << std::endl;
     }
 
     for (int i = 0; i < comm_sz - 1; ++i)
