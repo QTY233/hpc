649225920d95f6f1b19a0d2b91b98e6150954d53
QTY233 <2033545458@qq.com>
2025-04-13 17:37:35 +0800

    change back

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -14,50 +14,65 @@ void Worker::sort() {
         if (step == 0) std::sort(data, data + block_len);
         if (step & 1) {
             if (rank & 1) {
-                if (last_rank) continue;
                 MPI_Sendrecv(data, block_len, MPI_FLOAT, rank + 1, 0,
                     temp_data, block_len, MPI_FLOAT, rank + 1, 0,
                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if (temp_data[0] < data[block_len - 1]) {
+                    size_t i = 0, j = 0, k = 0;
+                    while (i < block_len && j < block_len) {
+                        if (data[i] < temp_data[j]) sorted_data[k++] = data[i++];
+                        else sorted_data[k++] = temp_data[j++];
+                    }
+                    while (i < block_len) sorted_data[k++] = data[i++];
+                    while (j < block_len) sorted_data[k++] = temp_data[j++];
+                    std::copy(sorted_data, sorted_data + block_len, data);
+                }
             } else {
                 if (!rank) continue;
                 MPI_Sendrecv(data, block_len, MPI_FLOAT, rank - 1, 0,
                     temp_data, block_len, MPI_FLOAT, rank - 1, 0,
                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if (data[0] < temp_data[block_len - 1]) {
+                    size_t i = 0, j = 0, k = 0;
+                    while (i < block_len && j < block_len) {
+                        if (data[i] < temp_data[j]) sorted_data[k++] = data[i++];
+                        else sorted_data[k++] = temp_data[j++];
+                    }
+                    while (i < block_len) sorted_data[k++] = data[i++];
+                    while (j < block_len) sorted_data[k++] = temp_data[j++];
+                    std::copy(sorted_data + block_len, sorted_data + (block_len << 1), data);
+                }
             }
         } else {
             if (rank & 1) {
                 MPI_Sendrecv(data, block_len, MPI_FLOAT, rank - 1, 0,
                     temp_data, block_len, MPI_FLOAT, rank - 1, 0,
                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if (data[0] < temp_data[block_len - 1]) {
+                    size_t i = 0, j = 0, k = 0;
+                    while (i < block_len && j < block_len) {
+                        if (data[i] < temp_data[j]) sorted_data[k++] = data[i++];
+                        else sorted_data[k++] = temp_data[j++];
+                    }
+                    while (i < block_len) sorted_data[k++] = data[i++];
+                    while (j < block_len) sorted_data[k++] = temp_data[j++];
+                    std::copy(sorted_data + block_len, sorted_data + (block_len << 1), data);
+                }
             } else {
                 if (last_rank) continue;
                 MPI_Sendrecv(data, block_len, MPI_FLOAT, rank + 1, 0,
                     temp_data, block_len, MPI_FLOAT, rank + 1, 0,
                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            }
-        }
-
-        if ((step + rank) & 1) {
-            if (data[0] < temp_data[block_len - 1]) {
-                size_t i = 0, j = 0, k = 0;
-                while (i < block_len && j < block_len) {
-                    if (data[i] < temp_data[j]) sorted_data[k++] = data[i++];
-                    else sorted_data[k++] = temp_data[j++];
+                if (temp_data[0] < data[block_len - 1]) {
+                    size_t i = 0, j = 0, k = 0;
+                    while (i < block_len && j < block_len) {
+                        if (data[i] < temp_data[j]) sorted_data[k++] = data[i++];
+                        else sorted_data[k++] = temp_data[j++];
+                    }
+                    while (i < block_len) sorted_data[k++] = data[i++];
+                    while (j < block_len) sorted_data[k++] = temp_data[j++];
+                    std::copy(sorted_data, sorted_data + block_len, data);
                 }
-                while (i < block_len) sorted_data[k++] = data[i++];
-                while (j < block_len) sorted_data[k++] = temp_data[j++];
-                std::copy(sorted_data + block_len, sorted_data + (block_len << 1), data);
-            }
-        } else {
-            if (temp_data[0] < data[block_len - 1]) {
-                size_t i = 0, j = 0, k = 0;
-                while (i < block_len && j < block_len) {
-                    if (data[i] < temp_data[j]) sorted_data[k++] = data[i++];
-                    else sorted_data[k++] = temp_data[j++];
-                }
-                while (i < block_len) sorted_data[k++] = data[i++];
-                while (j < block_len) sorted_data[k++] = temp_data[j++];
-                std::copy(sorted_data, sorted_data + block_len, data);
             }
         }
     }
