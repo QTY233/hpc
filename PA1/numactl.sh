7607ab8fdf5a884ad553a49100e00f08e7ea33eb
QTY233 <2033545458@qq.com>
2025-04-14 21:44:17 +0800

    543

--- PA1/numactl.sh
+++ PA1/numactl.sh
@@ -8,23 +8,18 @@ LOCAL_RANK=$SLURM_LOCALID # for SLURM
 # LOCAL_SIZE=$MPI_LOCALNRANKS # for Intel MPI
 LOCAL_SIZE=$SLURM_TASKS_PER_NODE # for SLURM
 
-NCPUS=$(nproc --all) # eg: 56 or 28 depending on the number of cores
-NUM_NUMA=2 # Number of NUMA nodes
+NCPUS=$(nproc --all) # eg: 28
+NUM_NUMA=2
 
-# Calculate binding parameters
-CORES_PER_PROCESS=$(($NCPUS / $LOCAL_SIZE)) # e.g., 7 when LOCAL_SIZE=4
-NUMA_ID=$(($LOCAL_RANK / $NUM_NUMA)) # determine which NUMA node the process belongs to
-NUMA_OFFSET=$(($LOCAL_RANK % $NUM_NUMA)) # offset within NUMA node
+# calculate binding parameters
+# bind to sequential cores in a NUMA domain
+CORES_PER_PROCESS=$(($NCPUS / $LOCAL_SIZE)) # eg: 28 when LOCAL_SIZE=28
+NUMA_ID=$(($LOCAL_RANK / ($NUM_NUMA * $CORES_PER_PROCESS))) # NUMA node assignment
+NUMA_OFFSET=$(($LOCAL_RANK % ($NUM_NUMA * $CORES_PER_PROCESS))) # Offset within NUMA node
+CORE_START=$(($NUMA_ID * $CORES_PER_PROCESS * $NUM_NUMA + $NUMA_OFFSET)) 
+CORE_END=$((($NUMA_ID + 1) * $CORES_PER_PROCESS * $NUM_NUMA - $NUM_NUMA + $NUMA_OFFSET))
+CORES=$(seq -s, $CORE_START $NUM_NUMA $CORE_END)
 
-# Calculate the starting and ending core IDs for each process
-CORE_START=$(($NUMA_ID * $CORES_PER_PROCESS * $NUM_NUMA + $NUMA_OFFSET)) # starting core for this rank
-CORE_END=$((($NUMA_ID + 1) * $CORES_PER_PROCESS * $NUM_NUMA - $NUM_NUMA + $NUMA_OFFSET)) # ending core for this rank
-
-# Generate a sequence of cores to bind the process to
-CORES=$(seq -s, $CORE_START $NUM_NUMA $CORE_END) # e.g., 0,2,4,6,8,10 for NUMA node 0
-
-# Debugging output to check binding info
+# execute command with specific cores
 echo "Process $LOCAL_RANK on $(hostname) bound to core $CORES"
-
-# Execute the program with numactl, binding to the specified cores
 exec numactl -C "$CORES" $@
