a56774b6fd77752aae10af05dcd5ab5015c0ccc3
QTY233 <2033545458@qq.com>
2025-04-14 23:19:57 +0800

    543

--- PA1/numactl.sh
+++ PA1/numactl.sh
@@ -13,12 +13,12 @@ NUM_NUMA=2
 
 # calculate binding parameters
 # bind to sequential cores in a NUMA domain
-CORES_PER_PROCESS=$(($NCPUS / $LOCAL_SIZE)) # eg: 28 when LOCAL_SIZE=28
-NUMA_ID=$(($LOCAL_RANK / ($NUM_NUMA * $CORES_PER_PROCESS))) # NUMA node assignment
-NUMA_OFFSET=$(($LOCAL_RANK % ($NUM_NUMA * $CORES_PER_PROCESS))) # Offset within NUMA node
-CORE_START=$(($NUMA_ID * $CORES_PER_PROCESS * $NUM_NUMA + $NUMA_OFFSET)) 
-CORE_END=$((($NUMA_ID + 1) * $CORES_PER_PROCESS * $NUM_NUMA - $NUM_NUMA + $NUMA_OFFSET))
-CORES=$(seq -s, $CORE_START $NUM_NUMA $CORE_END)
+CORES_PER_PROCESS=$(($NCPUS / $LOCAL_SIZE)) # eg: 7 when LOCAL_SIZE=4
+NUMA_ID=$(($LOCAL_RANK / $NUM_NUMA)) # eg: 0, 0, 1, 1
+NUMA_OFFSET=$(($LOCAL_RANK % $NUM_NUMA)) # 0, 1, 0, 1
+CORE_START=$(($NUMA_ID * $CORES_PER_PROCESS * $NUM_NUMA + $NUMA_OFFSET)) # eg: 0, 1, 14, 15
+CORE_END=$((($NUMA_ID + 1) * $CORES_PER_PROCESS * $NUM_NUMA - $NUM_NUMA + $NUMA_OFFSET)) # eg: 12, 13, 26, 27
+CORES=$(seq -s, $CORE_START $NUM_NUMA $CORE_END) # eg: 0,2,4,6,8,10,12 for rank 0
 
 # execute command with specific cores
 echo "Process $LOCAL_RANK on $(hostname) bound to core $CORES"
