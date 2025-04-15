6939c6ded67adf195546693398cd2168ab6b92d5
QTY233 <2033545458@qq.com>
2025-04-15 22:51:50 +0800

    123

--- PA1/run.sh
+++ PA1/run.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-# run on 1 machine * 28 process, feel free to change it!
-srun -N 2 -n 56 $*
+# 参数解析
+PROGRAM=$1
+N=$2
+DATAFILE=$3
 
+if [ -z "$PROGRAM" ] || [ -z "$N" ] || [ -z "$DATAFILE" ]; then
+    echo "用法: ./run.sh <程序名> <数据规模N> <数据文件路径>"
+    echo "示例: ./run.sh odd_even_sort 100 ./data/100.dat"
+    exit 1
+fi
+
+EXE=./$PROGRAM
+
+# 判断程序是否存在
+if [ ! -x "$EXE" ]; then
+    echo "错误：程序 $EXE 不存在或不可执行"
+    exit 2
+fi
+
+# 按规模分档运行
+if [ "$N" -lt 100 ]; then
+    srun -n 2 "$EXE" "$N" "$DATAFILE"
+elif [ "$N" -lt 10000 ]; then
+    srun -n 10 "$EXE" "$N" "$DATAFILE"
+else
+    srun -N 2 -n 56 --cpu-bind=none ./numactl.sh "$EXE" "$N" "$DATAFILE"
+fi
